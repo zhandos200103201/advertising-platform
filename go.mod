e904511da145bdf5dfe6c319882bb86fec8bba88
zhandos200103201 <saparbayevzh03@gmail.com>
2023-02-24 01:28:34 +0600

    authentication with login and signup web pages is done

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Goland
 
 go 1.20
 
-require github.com/go-sql-driver/mysql v1.7.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,51 +3,114 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"html/template"
-	"net/http"
 )
+import _ "github.com/go-sql-driver/mysql"
 
-var tpl *template.Template
+import "golang.org/x/crypto/bcrypt"
+
+import "net/http"
+
 var db *sql.DB
+var err error
+var tpl *template.Template
+
+func signupPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		tpl.ExecuteTemplate(res, "signup.html", nil)
+		return
+	}
+
+	username := req.FormValue("username")
+	password1 := req.FormValue("password1")
+	password2 := req.FormValue("password2")
+	email := req.FormValue("email")
+	name := req.FormValue("name")
+
+	var user string
+
+	err := db.QueryRow("SELECT username FROM users WHERE username=?", username).Scan(&user)
+
+	if password1 == password2 {
+		switch {
+		case err == sql.ErrNoRows:
+			hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password1), bcrypt.DefaultCost)
+			if err != nil {
+				http.Error(res, "Server error, unable to create your account.", 500)
+				return
+			}
+
+			_, err = db.Exec("INSERT INTO users(username, password, email, name) VALUES(?, ?, ?, ?)", username, hashedPassword, email, name)
+			if err != nil {
+				http.Error(res, "Server error, unable to create your account.", 500)
+				return
+			}
+
+			res.Write([]byte("User created!"))
+			return
+		case err != nil:
+			http.Error(res, "Server error, unable to create your account.", 500)
+			return
+		default:
+			http.Redirect(res, req, "/", 301)
+		}
+	} else {
+		http.Error(res, "Password doesn't match. Both passwords should be same!", 500)
+		return
+	}
+}
+
+func loginPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "POST" {
+		tpl.ExecuteTemplate(res, "login.html", nil)
+		return
+	}
+
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+
+	var databaseusername string
+	var databasePassword string
+
+	err := db.QueryRow("SELECT username, password FROM users WHERE username=?", username).Scan(&databaseusername, &databasePassword)
+
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		fmt.Println("Dont have any user")
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(databasePassword), []byte(password))
+	if err != nil {
+		http.Redirect(res, req, "/login", 301)
+		fmt.Println("Password is incorrect")
+		return
+	}
+
+	res.Write([]byte("Hello " + databaseusername))
+
+}
+
+func homePage(res http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(res, "index.html", nil)
+	return
+}
 
 func main() {
-	tpl, _ = template.ParseGlob("templates/*.html")
-
-	http.HandleFunc("/add_user", getAddUser)
-	http.HandleFunc("/", getIndex)
-	http.ListenAndServe("localhost:8081", nil)
-}
-
-func getIndex(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.html", nil)
-	return
-}
-
-func getAddUser(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		tpl.ExecuteTemplate(w, "add_user.html", nil)
-		r.ParseForm()
-	} else if r.Method == "POST" {
-		id := r.FormValue("id")
-		name := r.FormValue("name")
-		email := r.FormValue("email")
-		phone := r.FormValue("phone")
-
-		db, err := sql.Open("mysql", "zhandos:SAy#wm81j5AcM$Oy@tcp(localhost:3306)/go")
-		if err != nil {
-			fmt.Println("Error sql.Open")
-			panic(err.Error())
-		}
-		defer db.Close()
-
-		insert, err := db.Query(fmt.Sprintf("INSERT INTO `users`(`id`, `name`, `email`, `phone`) VALUES ('%s', '%s', '%s', '%s')", id, name, email, phone))
-		if err != nil {
-			panic(err.Error())
-		}
-		defer insert.Close()
-		http.Redirect(w, r, "/", 301)
+	tpl, _ = tpl.ParseGlob("templates/*.html")
+	db, err = sql.Open("mysql", "zhandos:SAy#wm81j5AcM$Oy@/go")
+	if err != nil {
+		panic(err.Error())
 	}
-	return
+	defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	http.HandleFunc("/signup", signupPage)
+	http.HandleFunc("/login", loginPage)
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8081", nil)
 }
